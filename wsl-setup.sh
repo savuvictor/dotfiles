61c243d37a03995500e2d398a7310ea2eb308d88
Victor Savu <savu.victoralexandru@gmail.com>
2025-06-18 23:45:43 +0300

    Updated script to install asdf

--- wsl-setup.sh
+++ wsl-setup.sh
@@ -17,5 +17,10 @@ wget https://go.dev/dl/go1.24.4.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf go1.24.4.linux-amd64.tar.gz
 
+go install github.com/asdf-vm/asdf/cmd/asdf@v0.18.0
+
 export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
+
+source ~/.bashrc
+
 
